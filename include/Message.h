439532010928aa1012122ae70f6ce1c7a78bcda5
Grey Johnson <grey.johnson@ufl.edu>
2020-11-02 20:18:53 -0500

    Standard Message Object

---
+++ include/Message.h
@@ -0,0 +1,22 @@
+#ifndef TB_MESSAGE
+#define TB_MESSAGE
+
+#include "IMessage.h"
+#include "MessageType.h"
+
+namespace TorrentialBits {
+    class Message : public IMessage {
+        public:
+            Message(MessageType _type, const std::vector<char>& _body);
+            Message(const std::vector<char>& _message);
+
+            const std::vector<char> GetRawMessage() const;
+
+            MessageType GetType() const;
+            const std::vector<char>& GetBody() const;
+        private:
+            MessageType type;
+            std::vector<char> body;
+    };
+}
+#endif
