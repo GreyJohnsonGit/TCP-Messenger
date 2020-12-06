a8103ad486d078349e5b93c6c7368735be82d5b3
Grey Johnson <grey.johnson@ufl.edu>
2020-12-06 14:33:52 -0500

    fixed odd, but correct, types

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -3,7 +3,7 @@
 using namespace TorrentialBits;
 
 std::vector<char> Utility::UintToCharVector(unsigned int value) {
-    int factor = sizeof(int)/sizeof(char);
+    int factor = sizeof(unsigned int)/sizeof(char);
     auto converted = std::vector<char>(factor);
     for (int i = 0; i < factor; i++) {
         converted[factor - i - 1] = value & (255 << (8 * i));
@@ -12,7 +12,7 @@ std::vector<char> Utility::UintToCharVector(unsigned int value) {
 }
 
 unsigned int Utility::UintToCharVector(std::vector<char> value) {
-    int factor = sizeof(int)/sizeof(char);
+    int factor = sizeof(unsigned int)/sizeof(char);
     int converted = 0;
     for (int i = 0; i < factor; i++) {
         converted += (unsigned int)value[i] << (8 * i);
