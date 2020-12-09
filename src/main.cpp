7945312246fbbe7768a621e8a607dff80bf0db60
Grey Johnson <grey.johnson@ufl.edu>
2020-12-09 10:39:57 -0500

    release directory

--- src/main.cpp
+++ src/main.cpp
@@ -11,46 +11,36 @@
 using namespace TorrentialBits;
 
 int main(int argc, char *argv[]) {
-    if(argc != 2) {
-        std::cout << "Command should be: make run <peerId>" << std::endl;
-        exit(-1);
+    try {
+        if(argc != 2) {
+            std::cout << "Command should be: make run <peerId>" << std::endl;
+            exit(-1);
+        }
+
+        int peerId = strtol(argv[1], nullptr, 10);
+        Defines defines = Defines("Common.cfg");
+        std::vector<PeerClient> peerClients = PeerClient::CreatePeersFromFile("PeerInfo0.cfg", &defines);
+
+        PeerClient peer = PeerClient::GetPeer(peerId, peerClients);
+
+        Server *peerServer = new Server(peer);
+        std::thread serverThread = peerServer->SpawnThread();
+
+        //TODO: Handle all choking/unchoking logic
+
+        for(PeerClient previousPeer : peerClients) {
+            std::cout << "Trying to create a client, current peer is: " << peerId << " and the previous is: " << previousPeer.GetPeerId() << "with listening port: " << previousPeer.GetListeningPort() << std::endl;
+            if(previousPeer.GetPeerId() == peerId) break;
+
+
+            Client *client = new Client(peer, previousPeer);
+            client->Connect();
+        }
+
+        serverThread.join();
     }
-
-    int peerId = strtol(argv[1], nullptr, 10);
-    Defines defines = Defines("Common.cfg");
-    std::vector<PeerClient> peerClients = PeerClient::CreatePeersFromFile("PeerInfo0.cfg", &defines);
-
-    PeerClient peer = PeerClient::GetPeer(peerId, peerClients);
-
-    Server *peerServer = new Server(peer);
-    std::thread serverThread = peerServer->SpawnThread();
-
-    //TODO: Handle all choking/unchoking logic
-
-    for(PeerClient previousPeer : peerClients) {
-        std::cout << "Trying to create a client, current peer is: " << peerId << " and the previous is: " << previousPeer.GetPeerId() << "with listening port: " << previousPeer.GetListeningPort() << std::endl;
-        if(previousPeer.GetPeerId() == peerId) break;
-
-
-        Client *client = new Client(peer, previousPeer);
-        client->Connect();
+    catch(const char* message) {
+        std::cout << message << std::endl;
     }
-
-    serverThread.join();
-
-//    try {
-//        Defines defines = Defines("Common.cfg");
-//        std::vector<PeerClient> peers = PeerClient::CreatePeersFromFile("PeerInfo0.cfg", &defines);
-//
-//        for (auto &peer : peers) {
-//            peer.Connect();
-//            auto message = Utility::StringToCharVector(std::to_string(peer.GetPeerId()));
-//            auto response = Utility::StringToCharVector(peer.Send(message));
-//            std::cout << response << std::endl;
-//        }
-//    }
-//    catch(const char* message) {
-//        std::cout << message << std::endl;
-//    }
     return 0;
 }
