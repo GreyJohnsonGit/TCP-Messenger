49c9f907d0d466478d515764b68282d582604d7b
Grey Johnson <grey.johnson@ufl.edu>
2020-12-20 17:34:40 -0500

    Added failed client connection feedback

--- src/Peer.cpp
+++ src/Peer.cpp
@@ -18,7 +18,10 @@ void Peer::Start()
 {
     int socketHandle = AttemptClientConnection();
     if (socketHandle == -1)
+    {
+        std::cout << "Listening..." << std::endl;
         socketHandle = AttemptServerConnection();
+    }
     
     std::cout << "Connection Successful..." << std::endl;
 
@@ -45,10 +48,10 @@ int Peer::AttemptClientConnection()
 
     struct pollfd connectionPoll = { socketHandle, POLLIN, 0};
 
-    if (poll(&connectionPoll, 1, 5000) && connect(socketHandle, (struct sockaddr*)&socketAddress, sizeof(socketAddress)) != -1)
+    if (connect(socketHandle, (struct sockaddr*)&socketAddress, sizeof(socketAddress)) == -1)
+        return  -1;
+    else
         return socketHandle;
-    
-    return  -1;
 }
 
 int Peer::AttemptServerConnection()
